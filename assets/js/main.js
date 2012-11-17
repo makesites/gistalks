96339d7e9cb2f6bf1d803d13b17c3ea67297232a
Makis Tracend <makis.tracend@gmail.com>
2012-11-17 02:10:31 -0800

    Adding Google Analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,17 @@ function init(){
 	
 //});
 }
+
+
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-16164477-8']);
+_gaq.push(['_setDomainName', 'makesit.es']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
